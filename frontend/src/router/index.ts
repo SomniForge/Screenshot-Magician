5a7fd21a1d30aaa7d4de26305d4e67fea21ef633
SebzIO <austinmcfarland83@gmail.com>
2025-04-14 13:53:00 -0400

    Feature: Started work on Screenshot Magician grid. Added Privacy Policy and Terms of Use routes, to be filled out soon.

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,8 +4,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 // Home View component
 import HomeView from '../views/HomeView.vue'
-// SS Magician Component
+// SS Magician component
 import ScreenshotMagician from '../views/ScreenshotMagician.vue'
+// Terms of Use component
+import TermsOfUse from '../views/TermsOfUse.vue'
+// Privacy Policy component
+import PrivacyPolicy from '../views/PrivacyPolicy.vue'
 
 // Create the router
 const router = createRouter({
@@ -19,7 +23,17 @@ const router = createRouter({
     {
       path: '/ssmag',
       name: 'screenshotmagician',
-      component: ScreenshotMagician // Point '/ssmag' to our new HomeView
+      component: ScreenshotMagician // Point '/ssmag' to the Screenshot Magician view
+    },
+    {
+      path: '/terms',
+      name: 'terms',
+      component: TermsOfUse // Point '/terms' to our Terms Of Use
+    },
+    {
+      path: '/privacy',
+      name: 'privacy',
+      component: PrivacyPolicy // Point '/privacy' to our Privacy Policy
     }
   ]
 })
--- frontend/src/views/ScreenshotMagician.vue
+++ frontend/src/views/ScreenshotMagician.vue
@@ -4,11 +4,130 @@
 </script>
 
 <template>
-  <div>
+  <div> <!-- Header Div-->
     <h2 id="mainHeader">Screenshot Magician</h2>
-    <p>Welcome!</p>    
-    <v-btn color="primary">Click Me</v-btn>
   </div>
+  <v-container class="bg-surface-variant">
+    <v-row no-gutters>
+      <v-row style="height: 70vh">
+        <v-sheet class="pa-2 ma-2 bg-surface-variant"> <!-- Left side buttons container -->
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          New Session
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Import Screenshot
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Import Chatlog
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Import Layer Image
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Image Drag
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Chat Drag
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Layer Drag
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Black Bars
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Create Layer
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Remove Layer
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Censor Selection
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Save Image
+          </v-btn>
+        </v-sheet>
+      </v-row>
+      <v-col>
+        <v-sheet class="pa-2 ma-2">
+          .v-col-auto
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
@@ -20,4 +139,10 @@ h2 {
 #mainHeader {
     text-align: center;
 }
+
+.v-btn {
+  margin-bottom: 20px;
+}
 </style>
+
+
