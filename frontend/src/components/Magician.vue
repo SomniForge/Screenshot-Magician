1787931f4de07d58cd61105c28f2e28176530118
SebzIO <austinmcfarland83@gmail.com>
2025-04-14 17:42:40 -0400

    Chore: Moved the actual screenshot magician code into a component.

---
+++ frontend/src/components/Magician.vue
@@ -0,0 +1,136 @@
+// src/components/Magician.vue
+
+<script setup>
+
+</script>
+
+<template>
+  <v-container class="bg-surface-variant">
+    <v-row no-gutters>
+      <v-row style="height: 70vh">
+        <v-sheet class="pa-2 ma-2 bg-surface-variant"> <!-- Left side buttons container -->
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          New Session
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Import Screenshot
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Import Chatlog
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Import Layer Image
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Image Drag
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Chat Drag
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Layer Drag
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Enable Black Bars
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Create Layer
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Remove Layer
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Censor Selection
+          </v-btn>
+          <v-btn
+          height="120"
+          max-width="180"
+          max-height="30"
+          color="black"
+          block
+          >
+          Save Image
+          </v-btn>
+        </v-sheet>
+      </v-row>
+      <v-col>
+        <v-sheet class="pa-2 ma-2">
+          .v-col-auto
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+
+</style>
+
+
+
